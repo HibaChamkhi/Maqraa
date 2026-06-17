e1007022278f97841d5aea99446cd4b638bd59a3
designdev <takiacademyzoom@gmail.com>
2026-06-17 15:45:50 +0000

    fix(firebase): populate real project config (maqraa-1add5) for web/android/ios

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -30,30 +30,34 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // TODO: replace all REPLACE_ME values via `flutterfire configure`.
+  // Values taken from android/app/google-services.json and
+  // ios/Runner/GoogleService-Info.plist (project: maqraa-1add5).
+  // NOTE: web reuses the Android API key. If you restricted that key in the
+  // Google Cloud console, register a Web app in the Firebase console and run
+  // `flutterfire configure` to get a dedicated web appId + browser key.
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'REPLACE_ME',
-    appId: 'REPLACE_ME',
-    messagingSenderId: 'REPLACE_ME',
-    projectId: 'REPLACE_ME',
-    authDomain: 'REPLACE_ME.firebaseapp.com',
-    storageBucket: 'REPLACE_ME.appspot.com',
+    apiKey: 'AIzaSyAthsPL2MKtMv0BA9fqhdW91SC3KIlb3lM',
+    appId: '1:173216165827:web:bb67b838dec899614cd776',
+    messagingSenderId: '173216165827',
+    projectId: 'maqraa-1add5',
+    authDomain: 'maqraa-1add5.firebaseapp.com',
+    storageBucket: 'maqraa-1add5.firebasestorage.app',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'REPLACE_ME',
-    appId: 'REPLACE_ME',
-    messagingSenderId: 'REPLACE_ME',
-    projectId: 'REPLACE_ME',
-    storageBucket: 'REPLACE_ME.appspot.com',
+    apiKey: 'AIzaSyAthsPL2MKtMv0BA9fqhdW91SC3KIlb3lM',
+    appId: '1:173216165827:android:bb67b838dec899614cd776',
+    messagingSenderId: '173216165827',
+    projectId: 'maqraa-1add5',
+    storageBucket: 'maqraa-1add5.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'REPLACE_ME',
-    appId: 'REPLACE_ME',
-    messagingSenderId: 'REPLACE_ME',
-    projectId: 'REPLACE_ME',
-    storageBucket: 'REPLACE_ME.appspot.com',
+    apiKey: 'AIzaSyBoqch-b9ZT3WFHZS400NGmWdlLpWRdmoI',
+    appId: '1:173216165827:ios:110767486257fc804cd776',
+    messagingSenderId: '173216165827',
+    projectId: 'maqraa-1add5',
+    storageBucket: 'maqraa-1add5.firebasestorage.app',
     iosBundleId: 'com.example.maqraa',
   );
 }
